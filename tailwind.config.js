72c0c3bd33ab27ab9c1d448a89332ea476c8df09
Alexandre Adiouma Ndene <alexandreadioumandene@esp.sn>
2022-04-16 00:14:44 +0200

    premier commit a faire

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    
+    extend: { colors: {
+      lightdark: "#32363f",
+      bolddark: "#2b2e37",
+      secondary: "#f45955",
+    },},
+  },
+  plugins: [],
+}
